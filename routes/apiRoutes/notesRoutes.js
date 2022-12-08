16ecf65155606b6e18faf8c36bfd860d839c8ebe
zain khawaja <zainkhawaja98@gmail.com>
2022-12-08 15:51:41 -0500

    finished index.js and server.js files and pseudocoded notesRoutes.js file

---
+++ routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,16 @@
+//get a note
+router.get('/notes', (req, res) => {
+
+});
+
+//post a note
+router.post('/notes', (req, res) => {
+
+});
+
+//delete a note
+router.delete('/notes/:id', (req, res) => {
+    
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+//initialize app and create port
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+//server starts on port
+app.listen(PORT, () => {
+    console.log(`API server listening on port ${PORT}!`);
+  });
